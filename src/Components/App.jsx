922d05f79d1c2cd999d91c03fd32cf37b5cde508
deepak bhatt <deepakbhattmits@gmail.com>
2019-10-05 08:04:47 +0530

    added new module

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import IdleTimer from 'react-idle-timer';
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 import createBrowserHistory from "./history";
 import Header from "./Header";
@@ -11,12 +12,28 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { userLogout } from "../Actions";
 class App extends Component {
+state={ timeout: 1000 * 5}
   handleClick = () => {
     this.props.userLogout();
   };
+  onActive = ()=>{
+    console.log('active');
+  }
+  onIdle = () => {
+    console.log('idle');
+    this.handleClick();
+  }
   render() {    
     return (
       <div className="App">
+         <IdleTimer
+        ref={ref => { this.idleTimer = ref }}
+        element={document}
+        onActive={this.onActive}
+        onIdle={this.onIdle}
+        onAction={this.onAction}
+        debounce={250}
+        timeout={this.state.timeout} />
         <ToastContainer autoClose={2000} />
         <Router history={createBrowserHistory}>
           {this.props.isSignedIn ? (
