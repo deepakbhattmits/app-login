6144d2c724d5e646425b5763528f9773c5e46e58
deepak bhatt <deepakbhattmits@gmail.com>
2019-10-22 07:01:14 +0530

    Dragable Component seperate

--- src/Components/Draggable.jsx
+++ src/Components/Draggable.jsx
@@ -1,64 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import ItemComponent from './ItemComponent'
 import VirtualDraggableGrid from 'react-virtual-draggable-grid';
-
-const ItemComponent = props => {
-    const { name, styles } = props;
-
-    return (
-        <div
-            style={{
-                userSelect: 'none',
-                border: '1px solid black',
-                fontFamily: 'sans-serif',
-                background: '#91c6a6',
-                ...styles,
-            }}
-        >
-            <p
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    margin: 0,
-                    width: '100%',
-                    height: '60%',
-                    fontSize: 18,
-                }}
-            >
-                {`Draggable ${name}!`}
-            </p>
-            <button
-                type="button"
-                style={{
-                    cursor: 'pointer',
-                    boxSizing: 'border-box',
-                    width: '100%',
-                    height: '40%',
-                    boxShadow: 'none',
-                    borderWidth: '1px 0 0 0',
-                    borderStyle: 'solid',
-                    borderColor: 'black',
-                    background: '#ccc',
-                    fontSize: 18,
-                }}
-                onClick={() => console.log('Clicked without initiating drag', name)}
-            >
-                {`Prevent Button Drag`}
-            </button>
-        </div>
-    );
-};
-
-ItemComponent.propTypes = {
-    name: PropTypes.string.isRequired,
-    styles: PropTypes.object,
-};
-
-ItemComponent.defaultProps = {
-    styles: {},
-};
-
 class Draggable extends React.Component {
     constructor(props) {
         super(props);
